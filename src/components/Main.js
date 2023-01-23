59222902e45718a5fd7323b34d66e1bf15fc7953
Mehtap Tataroğlu <teytap@gmail.com>
2023-01-23 09:43:25 +0300

    added meme Image function and some image css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import MemesData from "../MemesData";
+console.log(MemesData);
 export default function Main() {
+  const [memeImage, setMemeImage] = useState("/images/memeimg.png");
+  function getMemeImage(event) {
+    event.preventDefault();
+    setMemeImage(MemesData.data.memes[Math.floor(Math.random() * 100)].url);
+  }
+
   return (
     <main className="Main">
       <form>
         <input placeholder="Shut up" className="first-text" />
         <input placeholder="and take my money" className="secondText" />
-        <button>Get a new meme image 🖼</button>
+        <button onClick={getMemeImage} id="image-btn">
+          Get a new meme image 🖼
+        </button>
       </form>
-      <img src="/images/memeimg.png" alt="meme-img" />
+      <img src={memeImage} alt="meme-img" id="meme-image-place" />
+      <h1>Meme text</h1>
     </main>
   );
 }
