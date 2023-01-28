a6bb3595c652ded50b364c962d9ebf3e17fb93fe
Mehtap Tataroğlu <teytap@gmail.com>
2023-01-28 23:55:06 +0300

    added text on meme

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,23 +2,54 @@ import React, { useState } from "react";
 import MemesData from "../MemesData";
 console.log(MemesData);
 export default function Main() {
-  const [memeImage, setMemeImage] = useState("/images/memeimg.png");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "/images/memeimg.png",
+  });
+
   function getMemeImage(event) {
     event.preventDefault();
-    setMemeImage(MemesData.data.memes[Math.floor(Math.random() * 100)].url);
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: MemesData.data.memes[Math.floor(Math.random() * 100)].url,
+    }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({ ...prevMeme, [name]: value }));
+  }
   return (
     <main className="Main">
       <form>
-        <input placeholder="Shut up" className="first-text" />
-        <input placeholder="and take my money" className="secondText" />
+        <input
+          type="text"
+          placeholder="Top text"
+          className="first-text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="secondText"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button onClick={getMemeImage} id="image-btn">
           Get a new meme image 🖼
         </button>
       </form>
-      <img src={memeImage} alt="meme-img" id="meme-image-place" />
-      <h1>Meme text</h1>
+      <div className="meme">
+        <img src={meme.randomImage} alt="meme-img" id="meme-image-place" />
+
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
